331ca81206ee16f34beb253ec098ef5f8a67b52f
本馬遼太郎 <rajiku.blanc@gmail.com>
2022-11-05 15:52:04 +0900

    ログイン画面の作成

--- globals.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  // 環境変数名の定義
-  interface ProcessEnv {
-    /** 環境変数名と型 */
-    readonly NEXT_PUBLIC_SUPABASE_URL: string;
-    readonly SUPABASE_SECRET_KEY: string;
-  }
-}
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '../types/supabasetype';
-
-const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SECRET_KEY,
+const supabaseurl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+export const supabase = createClient<Database>(
+  supabaseurl,
+  supabaseAnonKey,
 );
